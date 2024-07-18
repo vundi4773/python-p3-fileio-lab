bdb3202605b8579c2b97d25ce5fab0b85e923698
vundi <vundi4773@gmail.com>
2024-07-18 13:23:37 +0300

    fileio

--- lib/file_io.py
+++ lib/file_io.py
@@ -1,8 +1,36 @@
+from pathlib import Path
+
 def write_file(file_name, file_content):
-    pass
+    if isinstance(file_name, Path):
+        file_name = str(file_name)
+
+    if not file_name.endswith('.txt'):
+        file_name +='.txt'
+    
+    with open(file_name,'w') as file:
+        file.write(file_content)
+    
 
 def append_file(file_name, append_content):
-    pass
+    if isinstance(file_name, Path):
+        file_name = str(file_name)
+
+    if not file_name.endswith('.txt'):
+        file_name +='.txt'
+    
+    with open(file_name,'a') as file:
+        file.write(append_content)
+    
+
 
 def read_file(file_name):
-    pass
+    if isinstance(file_name, Path):
+        file_name = str(file_name)
+    
+    if not file_name.endswith('.txt'):
+        file_name +='.txt'
+    
+    with open(file_name,'r') as file:
+        return file.read()
+    
+    
